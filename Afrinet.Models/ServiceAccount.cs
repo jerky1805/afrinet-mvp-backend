5d34f3062bdc8633ae06b7fc6ac45725d18e3265
Dapo Adisa <akindapo.adisa@gmail.com>
2023-12-01 14:42:57 +0000

    Added Model Class Library

---
+++ Afrinet.Models/ServiceAccount.cs
@@ -0,0 +1,9 @@
+﻿namespace Afrinet.Models;
+public class ServiceAccount
+{
+    public string? Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public DateTime LastLogin { get; set; }
+    public List<Service> Services { get; set; } = new List<Service>();
+}
---
+++ Afrinet.Models/UserAccount.cs
@@ -0,0 +1,23 @@
+﻿namespace Afrinet.Models;
+
+public class UserAccount
+{
+    public string? Id { get; set; }
+    public string? OtherNames { get; set; }
+    public string? Surname { get; set; }
+    public string? AddressLine1 { get; set; }
+    public string? AddressLine2 { get; set; }
+    public string? City { get; set; }
+    public string? State { get; set; }
+    public string? Country { get; set; }
+    public string? Email { get; set; }
+    public string? IDType { get; set; }
+    public string? IDNumber { get; set; }
+    public string? Language { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public DateTime LastLogin { get; set; }
+    public ServiceAccount ServiceAccount { get; set; } = new ServiceAccount();
+    public string? AccountType  { get; set; }
+
+}
